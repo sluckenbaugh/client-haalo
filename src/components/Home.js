70b12a5d0f97532970808af58f76e3d4253ac85a
Shane <shane@Shanes-MacBook-Pro-3.local>
2024-09-13 12:12:06 -0400

    Refactor question page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer.js'
 import Header from './Header.js';
+import laptop from '../_img/laptop.jpg'
 
 const Home = () => {
     // const images = require.context('../_img/cards/v2', true);
@@ -9,28 +10,30 @@ const Home = () => {
     return (
         <div className='bg-companyGray'>
             <Header />
-            <div id="step1" className="bg-white px-[7rem] py-[3rem] m-[1.5rem] rounded-[8px]">
-                <div className='flex justify-center'>
-                    <div class="flex justify-between w-[30%] items-center">
-                        <div className="step_selected">
-                            <p className='step_num'>Step 1</p>
-                        </div>
-                        <div className="step_line" />
-                        <div className="step_unselected">
-                            <p className='step_num'>Step 2</p>
-                        </div>
-                        <div className="step_line" />
-                        <div className="step_unselected">
-                            <p className='step_num'>Step 3</p>
+            <main className='px-[6rem]'>
+                <div id="step1" className="bg-white px-[7rem] py-[3rem] my-[1.5rem] rounded-[8px] grid">
+                    <div className='flex justify-center'>
+                        <div class="flex justify-between w-[30%] items-center">
+                            <div className="step_selected">
+                                <p className='step_num'>Step 1</p>
+                            </div>
+                            <div className="step_line" />
+                            <div className="step_unselected">
+                                <p className='step_num'>Step 2</p>
+                            </div>
+                            <div className="step_line" />
+                            <div className="step_unselected">
+                                <p className='step_num'>Step 3</p>
+                            </div>
                         </div>
                     </div>
+                    <WelcomeMessage />
+                    <a href="/test" className='justify-self-center mt-[2rem]'><button name="starttest" value="Take The Halo Test" className="button">Launch the HAALO Inventory</button></a>
                 </div>
-                <WelcomeMessage />
-            </div>
-
-                <a href="/test"><button name="starttest" value="Take The Halo Test" className="button">Launch the HAALO Inventory</button></a>
-                <br /><br /><br />
-                <AboutHAALO />
+                <div className='bg-white px-[4rem] py-[1rem] my-[2rem]  rounded-[8px] grid'>
+                    <AboutHAALO />
+                </div>
+            </main>
             <Footer />
         </div>
     );
@@ -40,20 +43,21 @@ export default Home;
 
 const WelcomeMessage = () => {
     return (
-        <div className='flex flex-col items-center justify-center h-[130px]'>
-            <br />
-            <h2 className=''>Welcome to HAALO</h2>
-            <p className="text-center">Whether it's making a difference, being part of a team or seeing your name in lights, each of us have preferences and undeniable quirks that simply cannot be captured by a resume. HAALO allows us to better understand who you are, what you want, and how we can help.</p>
+        <div className='flex flex-col items-center justify-center w-[50%] mx-auto mt-[1.5rem]'>
+            <h2 className='mb-[1rem] header'>Welcome to HAALO</h2>
+            <p className="text-center text-lg">Whether it's making a difference, being part of a team or seeing your name in lights, each of us have preferences and undeniable quirks that simply cannot be captured by a resume. HAALO allows us to better understand who you are, what you want, and how we can help.</p>
         </div>
     )
 }
 
 const AboutHAALO = () => {
     return (
-        <div className='flex flex-col items-center justify-center h-[130px]'>
-            <br />
-            <h3 className='title2'>About the HAALO Persona Inventory</h3>
-            <p className="instructions">HAALO stems from an in-depth exploration of 4 organizational cultures (outlined below), delineating various working styles, motivations, and other key aspects. Through this study, we've developed a comprehensive inventory of statements, focused through a people-centric perspective. This inventory offers insights into how individuals may align within an organization or specific roles.</p>
-        </div>
+            <div className='grid grid-cols-2 my-[2.5rem] gap-12'>
+                <div className='flex flex-col'>
+                    <h3 className='mb-[1rem] header'>What is HAALO?</h3>
+                    <p className=" text-lg">HAALO stems from an in-depth exploration of 4 organizational cultures (outlined below), delineating various working styles, motivations, and other key aspects. Through this study, we've developed a comprehensive inventory of statements, focused through a people-centric perspective. This inventory offers insights into how individuals may align within an organization or specific roles.</p>
+                </div>
+                <img className='w-[100%] justify-self-end rounded-[40px]' src={laptop}></img>
+            </div>
     )
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -16,22 +16,22 @@ const Questions = () => {
     });
 
     const [questionIndex, setQuestionIndex] = useState(0);
-    const [yourName, setYourName] = useState({
-        firstName: '',
-        lastName: ''
-    });
-    const [yourEmail, setYourEmail] = useState('');
+    // const [yourName, setYourName] = useState({
+    //     firstName: '',
+    //     lastName: ''
+    // });
+    // const [yourEmail, setYourEmail] = useState('');
 
-    const handleNameChange = (e) => {
-        if (e.target.name === "firstName")  {
-            setYourName({...yourName, firstName: e.target.value})
-        }
-        else setYourName({...yourName, lastName: e.target.value})
-    }
+    // const handleNameChange = (e) => {
+    //     if (e.target.name === "firstName")  {
+    //         setYourName({...yourName, firstName: e.target.value})
+    //     }
+    //     else setYourName({...yourName, lastName: e.target.value})
+    // }
 
-    const handleEmailChange = (e) => {
-        setYourEmail(e.target.value.toLowerCase())
-    }
+    // const handleEmailChange = (e) => {
+    //     setYourEmail(e.target.value.toLowerCase())
+    // }
 
     const handleNextQuestion = () => {
         if (questionIndex <= haaloData.length - 1 && choices[questionIndex].option != null) {
@@ -53,80 +53,80 @@ const Questions = () => {
         handleNextQuestion()
     };
 
-    const validEmail = () => {
-        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return re.test(String(yourEmail).toLowerCase());
-    }
+    // const validEmail = () => {
+    //     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    //     return re.test(String(yourEmail).toLowerCase());
+    // }
 
     const navigate = useNavigate()
 
     const takeToResults = (e) => {
         e.preventDefault()
-        if (answeredAllQuestions() && validEmail() && yourName.firstName !== '' && yourName.lastName !== '') {
+        if (answeredAllQuestions()) {
             const persona = PersonaService.findPersona(choices)
             navigate('/results', { state: { persona } })
         }
+        // if (answeredAllQuestions() && validEmail() && yourName.firstName !== '' && yourName.lastName !== '') {
+        //     const persona = PersonaService.findPersona(choices)
+        //     navigate('/results', { state: { persona } })
+        // }
     }
 
     return (
-        <div>
+        <div className='bg-companyGray'>
             <Header />
-            <div id="step2" className="container">
-                <br></br>
-                {/* Step 1 bubble */}
-                <div className="step_unselected">
-                    <p className='step_num'>Step 1</p>
-                </div>
-
-                <div className="step_line" />
-
-                {/* Step 2 bubble */}
-                <div className="step_selected">
-                    <p className='step_num'>Step 2</p>
-                </div>
-
-                <div className="step_line" />
-
-                {/* Step 3 bubble */}
-                <div className="step_unselected">
-                    <p className='step_num'>Step 3</p>
-                </div>
-                <Instructions />
-                <form onSubmit={(e) => takeToResults(e)}>
-                    <input type="hidden" name="subject" value="HAALO Results Submission" />
-                    <input type="hidden" name="recipient" value="haalo@avenica.com" />
-
-                    <div className="question">
-                        <label htmlFor="firstName">First Name:</label>
-                        <input type="text" required="required" onChange={handleNameChange} id="firstName" name="firstName" />
-                    </div>
-
-                    <div className="question">
-                        <label htmlFor="lastName">Last Name:</label>
-                        <input type="text" required="required"  onChange={handleNameChange} id="lastName" name="lastName" />
-                    </div>
-
-                    <div className="question">
-                        <label htmlFor="email">Your Email:</label>
-                        <input type="text" required="required" value={yourEmail} onChange={handleEmailChange} id="email" name="email" />
-                    </div>
-
-                    {haaloData.map((question, index) => (
-                        <div className="question" key={index}>
-                            {`Question ${index + 1}:`}
-                            <br/>
-                                {question.options.map((option, optIndex) => (
-                                    <div key={optIndex} onClick={() => handleResponseChange(option, optIndex, index)}>
-                                        <input style={{marginRight: '5px'}} type="radio" required="required" name={`question${index+1}`}/>
-                                        <label>{option.statement}</label> 
-                                    </div>
-                                ))}
+            <main className='px-[6rem]'>
+                    <div id="step2" className="bg-white px-[7rem] py-[3rem] my-[1.5rem] rounded-[8px] grid">
+                        <div className='flex justify-center'>
+                            <div class="flex justify-between w-[30%] items-center">
+                                <div className="step_unselected">
+                                    <p className='step_num'>Step 1</p>
+                                </div>
+                                <div className="step_line" />
+                                <div className="step_selected">
+                                    <p className='step_num'>Step 2</p>
+                                </div>
+                                <div className="step_line" />
+                                <div className="step_unselected">
+                                    <p className='step_num'>Step 3</p>
+                                </div>
+                            </div>
                         </div>
-                    ))}
-
-                    <button className='button' type='submit'>Discover My Persona</button>
-                </form>
-            </div>
+                        <Instructions />
+                    </div>
+                    <div className='bg-white px-[7rem] py-[3rem] my-[1.5rem] rounded-[8px] grid justify-center'>
+                        <form className='grid' onSubmit={(e) => takeToResults(e)}>
+                            <input type="hidden" name="subject" value="HAALO Results Submission" />
+                            <input type="hidden" name="recipient" value="haalo@avenica.com" />
+                            {/* <div className="question">
+                                <label htmlFor="firstName">First Name:</label>
+                                <input type="text" required="required" onChange={handleNameChange} id="firstName" name="firstName" />
+                            </div>
+                            <div className="question">
+                                <label htmlFor="lastName">Last Name:</label>
+                                <input type="text" required="required"  onChange={handleNameChange} id="lastName" name="lastName" />
+                            </div>
+                            <div className="question">
+                                <label htmlFor="email">Your Email:</label>
+                                <input type="text" required="required" value={yourEmail} onChange={handleEmailChange} id="email" name="email" />
+                            </div> */}
+                            <h3 className='text-companyBlue text-[28px] mb-[1rem]'>Choose the answer that best descibes you.</h3>
+                            {haaloData.map((question, index) => (
+                                <div className="text-lg" key={index}>
+                                    <p className='text-xl mb-[0.5rem] mt-[1rem]'>{`Question ${index + 1}:`}</p>
+                                        {question.options.map((option, optIndex) => (
+                                            <div key={optIndex} onClick={() => handleResponseChange(option, optIndex, index)}>
+                                                <input className='mr-[0.6rem]' type="radio" required="required" name={`question${index+1}`}/>
+                                                <label>{option.statement}</label>
+                                            </div>
+                                        ))}
+                                        <div className="question_line" />
+                                </div>
+                            ))}
+                        <button className='button justify-self-center' type='submit'>Discover My Persona</button>
+                        </form>
+                    </div>
+            </main>
             <Footer />
         </div>
     );
@@ -136,11 +136,9 @@ export default Questions;
 
 const Instructions = () => {
     return (
-        <div className='flex flex-col items-center justify-center mt-5'>
-            <h2 className='title1' style={{marginTop: '1%'}}>Instructions:</h2>
-            <p className="instructions">Please select the answer that best aligns with you. If neither answer represents you, choose the one that feels closest to your gut reaction.<br />It typically takes 20 minutes to complete the inventory.</p>
-            <br />
-            <h3 className='title2'>Choose The Answer That Best Describes You:</h3>
+        <div className='flex flex-col items-center justify-center mt-[1.5rem] mx-[10rem]'>
+            <h2 className='header mb-[0.7rem]'>Instructions:</h2>
+            <p className="text-center text-lg">Please select the answer that best aligns with you. If neither answer represents you, choose the one that feels closest to your gut reaction.<br />It typically takes around 10 minutes to complete the inventory.</p>
         </div>   
     )
 }
